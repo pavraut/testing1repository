f3fa75ed6b294df0bee8f524c9a3847c9518bf8c
HP <HP@LAPTOP-SHEQ3I3F>
2023-03-09 23:22:29 +0530

    by test1

---
+++ Testing_Git2/src/test_git2/Git1.java
@@ -0,0 +1,11 @@
+package test_git2;
+
+public class Git1 
+{
+	public static void main(String[] args) {
+		
+		System.out.println("Git1----");
+		
+	}
+
+}
---
+++ Testing_Git2/src/test_git2/Git2.java
@@ -0,0 +1,11 @@
+package test_git2;
+
+public class Git2 
+{
+	public static void main(String[] args) {
+		
+		System.out.println("Git2----");
+		
+	}
+
+}
